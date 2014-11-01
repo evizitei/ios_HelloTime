ec22907332ba1dc32564214f3b604377d3bf292b
Ethan Vizitei <ethan.vizitei@gmail.com>
2014-11-01 14:33:25 -0600

    Initial Commit

---
+++ HelloTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloTime
+//
+//  Created by Ethan Vizitei on 11/1/14.
+//  Copyright (c) 2014 Ethan Vizitei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
