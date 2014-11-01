9a14d14c1de5da7606bf29ac60ca4c49178ed382
Ethan Vizitei <ethan.vizitei@gmail.com>
2014-11-01 15:13:24 -0600

    create hello time application from iOS7 in Action
    (ported to iOS8.1 & Swift)

--- HelloTime/ViewController.swift
+++ HelloTime/ViewController.swift
@@ -13,13 +13,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.checkTime()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func checkTime() {
+        var formatter = NSDateFormatter()
+        let date = NSDate()
+        formatter.dateFormat = "h:mm:ss a"
+        timeLabel.text = formatter.stringFromDate(date)
+
+        NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: Selector("checkTime"), userInfo: nil, repeats: false)
+    }
 
 
+    @IBOutlet weak var timeLabel: UILabel!
 }
 
